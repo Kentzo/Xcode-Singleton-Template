9a440b872a3203b4fc89a6e3c208c8332d38285b
Ilya Kulakov <kulakov.ilya@gmail.com>
2010-08-14 22:26:11 +0700

    init

---
+++ Singleton Subclass.pbfiletemplate/class.h
@@ -0,0 +1,18 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//	File created using Singleton XCode Template by Mugunth Kumar (http://mugunthkumar.com
+//  Permission granted to do anything, commercial/non-commercial with this file apart from removing the line/URL above
+
+#import <Foundation/Foundation.h>
+
+@interface «FILEBASENAMEASIDENTIFIER» : NSObject {
+
+}
+
++ («FILEBASENAMEASIDENTIFIER»*) sharedInstance;
+
+@end
---
+++ Singleton Subclass.pbfiletemplate/class.m
@@ -0,0 +1,65 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//	File created using Singleton XCode Template by Mugunth Kumar (http://mugunthkumar.com
+//  Permission granted to do anything, commercial/non-commercial with this file apart from removing the line/URL above
+
+«OPTIONALHEADERIMPORTLINE»
+
+static «FILEBASENAMEASIDENTIFIER» *_instance;
+@implementation «FILEBASENAMEASIDENTIFIER»
+
++ («FILEBASENAMEASIDENTIFIER»*)sharedInstance
+{
+	@synchronized(self) {
+		
+        if (_instance == nil) {
+			
+            _instance = [[super allocWithZone:NULL] init];
+            
+            // Allocate/initialize any member variables of the singleton class her
+            // example
+			//_instance.member = @"";
+        }
+    }
+    return _instance;
+}
+
+
+#pragma mark Singleton Methods
+
++ (id)allocWithZone:(NSZone *)zone
+{	
+	return [[self sharedInstance]retain];	
+}
+
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return self;	
+}
+
+- (id)retain
+{	
+    return self;	
+}
+
+- (unsigned)retainCount
+{
+    return NSUIntegerMax;  //denotes an object that cannot be released
+}
+
+- (void)release
+{
+    //do nothing
+}
+
+- (id)autorelease
+{
+    return self;	
+}
+
+@end
