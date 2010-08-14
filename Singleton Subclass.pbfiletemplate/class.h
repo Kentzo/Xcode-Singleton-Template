8c2be5d50daa2bc39b67ade4bbb16048ac71d308
Ilya Kulakov <kulakov.ilya@gmail.com>
2010-08-14 23:53:44 +0700

    edit format

--- Singleton Subclass.pbfiletemplate/class.h
+++ Singleton Subclass.pbfiletemplate/class.h
@@ -4,15 +4,19 @@
 //
 //  Created by «FULLUSERNAME» on «DATE».
 //  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
-//	File created using Singleton XCode Template by Mugunth Kumar (http://mugunthkumar.com
-//  Permission granted to do anything, commercial/non-commercial with this file apart from removing the line/URL above
 
 #import <Foundation/Foundation.h>
+
 
 @interface «FILEBASENAMEASIDENTIFIER» : NSObject {
 
 }
 
-+ («FILEBASENAMEASIDENTIFIER»*) sharedInstance;
+@end
+
+
+@interface «FILEBASENAMEASIDENTIFIER» (Singleton)
+
++ («FILEBASENAMEASIDENTIFIER» *) sharedInstance;
 
 @end
--- Singleton Subclass.pbfiletemplate/class.m
+++ Singleton Subclass.pbfiletemplate/class.m
@@ -4,16 +4,15 @@
 //
 //  Created by «FULLUSERNAME» on «DATE».
 //  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
-//	File created using Singleton XCode Template by Mugunth Kumar (http://mugunthkumar.com
-//  Permission granted to do anything, commercial/non-commercial with this file apart from removing the line/URL above
 
 «OPTIONALHEADERIMPORTLINE»
 
-static «FILEBASENAMEASIDENTIFIER» *_instance;
-@implementation «FILEBASENAMEASIDENTIFIER»
 
-+ («FILEBASENAMEASIDENTIFIER»*)sharedInstance
-{
+static «FILEBASENAMEASIDENTIFIER» *_instance;
+
+@implementation «FILEBASENAMEASIDENTIFIER» (Singleton)
+
++ («FILEBASENAMEASIDENTIFIER» *)sharedInstance {
 	@synchronized(self) {
 		
         if (_instance == nil) {
@@ -31,35 +30,34 @@ static «FILEBASENAMEASIDENTIFIER» *_instance;
 
 #pragma mark Singleton Methods
 
-+ (id)allocWithZone:(NSZone *)zone
-{	
++ (id)allocWithZone:(NSZone *)zone {	
 	return [[self sharedInstance]retain];	
 }
 
 
-- (id)copyWithZone:(NSZone *)zone
-{
+- (id)copyWithZone:(NSZone *)zone {
     return self;	
 }
 
-- (id)retain
-{	
+- (id)retain {	
     return self;	
 }
 
-- (unsigned)retainCount
-{
+- (unsigned)retainCount {
     return NSUIntegerMax;  //denotes an object that cannot be released
 }
 
-- (void)release
-{
+- (void)release {
     //do nothing
 }
 
-- (id)autorelease
-{
+- (id)autorelease {
     return self;	
 }
+
+@end
+
+
+@implementation «FILEBASENAMEASIDENTIFIER»
 
 @end
